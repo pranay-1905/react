44e970e55a283cb31ec4d9e69f7735fe621e3778
pranay-1905 <pranaychikankar2@gmail.com>
2025-06-22 22:53:14 +0530

    context api

---
+++ 08miniContex/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+      <h1>pranay chikankar mini context</h1>
+      <Login />
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
